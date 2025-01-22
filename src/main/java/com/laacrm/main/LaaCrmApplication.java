3c3b685775a127806df9557ac8470d62a80f4197
shyamsundar.vj <shyamsundar.vj@zohocorp.com>
2025-01-22 15:54:47 +0530

    Issue fix

--- src/main/java/com/laacrm/main/LaaCrmApplication.java
+++ src/main/java/com/laacrm/main/LaaCrmApplication.java
@@ -1,6 +1,5 @@
 package com.laacrm.main;
 
-import com.laacrm.main.core.service.TemplateDataPopulate;
 import com.laacrm.main.framework.service.initPopulate.InitialPopulationService;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
@@ -12,12 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaaCrmApplication {
 
 	private final InitialPopulationService initPopulationService;
-	private final TemplateDataPopulate templateDataPopulate;
 
 	@PostConstruct
 	public void init() throws Exception {
 		initPopulationService.initPopulate();
-		templateDataPopulate.populate();
 	}
 
 	public static void main(String[] args) {
